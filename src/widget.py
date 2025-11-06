c9c62d5c3daf7376b17b1e79b966317ec24e9ec0
Alexander <sanya.core@gmail.com>
2025-11-06 21:54:28 +0300

    function get_date has been added to widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 import masks
 
+
 def mask_account_card(type_number: str) -> str:
     """Принимает тип и номер карты или счета и возвращает замаскированный номер"""
     if type_number.startswith("Счет"):
@@ -8,6 +9,10 @@ def mask_account_card(type_number: str) -> str:
     else:
         number = type_number[-16:] #выделяем номер из строки
         mask_account = type_number[:-16] + masks.get_mask_card_number(number)
-
     return mask_account
 
+
+def get_date(date_string: str) -> str:
+    """Принимает строку с датой и возвращает дату в заданном формате"""
+    formatted_date = date_string[8:10] + "." + date_string[5:7] + "." + date_string[:4]
+    return formatted_date
