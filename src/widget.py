129a958ba7a685fe8969b158fb9f36591df9ee4d
Alexander <sanya.core@gmail.com>
2025-11-09 19:06:39 +0300

    Corrections have been made regarding imports

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,14 @@
-import masks
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(type_number: str) -> str:
     """Принимает тип и номер карты или счета и возвращает замаскированный номер"""
     if type_number.startswith("Счет"):
         number = type_number[6:]  # выделяем номер из строки
-        mask_account = type_number[:5] + masks.get_mask_account(number)
+        mask_account = type_number[:5] + get_mask_account(number)
     else:
         number = type_number[-16:]  # выделяем номер из строки
-        mask_account = type_number[:-16] + masks.get_mask_card_number(number)
+        mask_account = type_number[:-16] + get_mask_card_number(number)
     return mask_account
 
 
