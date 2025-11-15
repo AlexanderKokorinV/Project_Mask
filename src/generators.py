e531e77260e7ec870ac6a96b27482b4864dac646
Alexander <sanya.core@gmail.com>
2025-11-15 22:35:07 +0300

    function transaction_descriptions has been added to generators.py

--- src/generators.py
+++ src/generators.py
@@ -8,3 +8,9 @@ def filter_by_currency(transactions: List[Dict], currency: str) -> Generator[Dic
         if transaction.get("operationAmount").get("currency").get("code") == currency:
             yield transaction
 
+def transaction_descriptions(transactions: List[Dict]) -> Generator[str]:
+    """Функция-генератор, которая принимает список словарей с транзакциями и
+    возвращает описание каждой операции по очереди"""
+    for transaction in transactions:
+        transaction_description = transaction.get("description")
+        yield transaction_description
