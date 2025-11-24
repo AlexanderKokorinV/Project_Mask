44e3d76947515af26279e62f0ff1a8c10e30f35e
Alexander <sanya.core@gmail.com>
2025-11-24 23:20:46 +0300

    tests for external_api have been added

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,14 @@
 import unittest
-from unittest.mock import Mock, mock_open, patch
+from unittest.mock import mock_open, patch
 import json
 from src.utils import get_transactions_from_json
 
 class TestGetTransactionsFromJson(unittest.TestCase):
-        MOCK_DATA = [                # Mock-данные, которые мы будем использовать в тестах
+        MOCK_DATA = [                #Mock-данные, которые мы будем использовать в тестах
             {"id": 123, "amount": 10},
             {"id": 345, "amount": 20}
         ]
-        MOCK_JSON_STR = json.dumps(MOCK_DATA) ## Строковое представление mock-данных для имитации содержимого файла
+        MOCK_JSON_STR = json.dumps(MOCK_DATA) #Строковое представление mock-данных для имитации содержимого файла
         @patch("builtins.open", new_callable=mock_open)
         def test_get_transactions_from_json(self, mock_file_open):
             """
@@ -31,21 +31,22 @@ class TestGetTransactionsFromJson(unittest.TestCase):
             self.assertEqual(result, []) # Ожидаем пустой список в случае ошибки
             mock_file_open.assert_called()  #Проверяем, что open вызывался
 
-        @patch("builtins.open'", new_callable=mock_open)
-        def test_invalid_json_format(self, mock_file_open):
+        @patch("builtins.open", new_callable=mock_open)
+        def test_json_decode_error(self, mock_file_open):
             """
             Тест обработки ситуации, когда JSON файл поврежден (json.JSONDecodeError).
             """
             mock_file_open.return_value.read.return_value = "Это невалидный JSON" #Имитируем файл, содержащий невалидный JSON
+
             result = get_transactions_from_json("invalid/file.json")
 
             self.assertEqual(result, []) #Ожидаем пустой список в случае ошибки декодирования
-            mock_file_open.assert_called()
+            mock_file_open.assert_called_with("invalid/file.json", encoding="utf-8")
 
         @patch("builtins.open", new_callable=mock_open)
         def test_empty_file_handling(self, mock_file_open):
             """
-            Тест обработки пустого файла, который вызывает ValueError или JSONDecodeError.
+            Тест обработки пустого файла, который вызывает ValueError.
             """
             mock_file_open.return_value.read.return_value = "" #Имитируем пустой файл
             result = get_transactions_from_json("empty/file.json")
