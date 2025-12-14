c60ef5ceb0461380d5fa0a114aff0ececef03045
Alexander <sanya.core@gmail.com>
2025-12-14 22:25:37 +0300

    logging objects have been added for modules utils.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,22 @@
+import logging
+
+masks_logger = logging.getLogger("masks")
+masks_logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
+
+
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает его замаскированную версию"""
     if card_number.isdigit() and len(card_number) == 16:
         mask_number = card_number[:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[-4:]
+        masks_logger.info(f"Принимаем на вход номер карты {card_number} и возвращаем его в замаскированном виде {mask_number}")
     else:
+        masks_logger.error("Ошибка: некорректный формат номера карты")
         mask_number = "Ошибка"
     return mask_number
 
@@ -11,6 +25,8 @@ def get_mask_account(account: str) -> str:
     """Принимает на вход номер банковского счета и возвращает его замаскированную версию"""
     if account.isdigit() and len(account) == 20:
         mask_account = "**" + account[-4:]
+        masks_logger.info(f"Принимаем на вход номер банковского счета {account} и возвращаем его а замаскированном виде {mask_account}")
     else:
+        masks_logger.error("Ошибка: некорректный формат номера банковского счета")
         mask_account = "Ошибка"
     return mask_account
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,18 @@
 import json
 import os
+import sys
 from typing import Any
+import logging
 
+utils_logger = logging.getLogger("utils")
+utils_logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+utils_logger.addHandler(file_handler)
+
+
+utils_logger.info("Формируем путь к файлу operations.json")
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))  # Определяем путь к корневой папке проекта
 PATH_TO_JASON_FILE = os.path.join(ROOT_DIR, "data", "operations.json")  # Формируем путь к файлу operations.json
 
@@ -13,12 +24,16 @@ def get_transactions_from_json(PATH_TO_JASON_FILE: str) -> Any:
     функция возвращает пустой список
     """
     try:
+        utils_logger.info(f"Принимаем путь к файлу operations.json и возвращаем список словарей с данными о финансовых транзакциях")
         with open(PATH_TO_JASON_FILE, encoding="utf-8") as file:
             list_transactions = json.load(file)
             return list_transactions
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        utils_logger.error("Ошибка: файл не найден")
         return []
     except ValueError:
+        utils_logger.error("Ошибка: файл пуст, либо содержит не список")
         return []
     except json.JSONDecodeError:
+        utils_logger.error("Ошибка: файл содержит некорректные данные")
         return []
